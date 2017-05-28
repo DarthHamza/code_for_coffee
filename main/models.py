9a77ebccf3b96e3a983e329920ca9814fb15be4e
DarthHamza <hamzamakia@gmail.com>
2017-05-28 14:00:34 +0300

    created models for everything needed to make a coffee

--- main/models.py
+++ main/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Bean(models.Model):
+	name = models.CharField(max_length=255)
+
+	def __str__(self):
+		return self.name
+
+class Roast(models.Model):
+	name = models.CharField(max_length=255)
+
+	def __str__(self):
+		return self.name
+
+class Powder(models.Model):
+	name = models.CharField(max_length=255)
+
+	def __str__(self):
+		return self.name
+
+class Syrup(models.Model):
+	name = models.CharField(max_length=255)
+
+	def __str__(self):
+		return self.name
+
+
+class Coffee(models.Model):
+	name = models.CharField(max_length=255)
+	bean = models.ForeignKey(Bean)
+	roast = models.ForeignKey(Roast)
+	espresso_shots = models.PositiveIntegerField(default=1)
+	water = models.FloatField(default=0)
+	steamed_milk = models.BooleanField(default=False)
+	micro_foam = models.FloatField(null=True, blank=True)
+	powder = models.ManyToManyField(Powder, null=True, blank=True)
+	syrup = models.ManyToManyField(Syrup, null=True, blank=True)
+	extra_instructions = models.TextField(null=True, blank=True)
+	def __str__(self):
+		return self.name
